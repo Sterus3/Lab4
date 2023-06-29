4b5cd7767da7a4b882cba732b40517f968b7a928
Dsw51305 <dsw51305@student.dsw.edu.pl>
2023-06-29 09:27:09 +0200

    Dodano flagę -e/--error, która tworzy errorx/errorx.txt, bez liczby ma domyślne tworzyć 100 plików, lub z podaną liczbą tworzy odpowiednią liczbę plików i katalogów

--- skrypt.sh
+++ skrypt.sh
@@ -20,11 +20,24 @@ elif [[ $1 == "-l" || $1 == "--logs" ]]; then
 
 elif [[ $1 == "-h" || $1 == "--help" ]]; then
   echo "Dostępne opcje:"
-  echo "# --date: Wyświetla dzisiejszą datę."
-  echo "# --logs <liczba_plikow>: Tworzy określoną liczbę plików log."
-  echo "# --help: Wyświetla wszystkie dostępne opcje."
+  echo "# -d/--date: Wyświetla dzisiejszą datę."
+  echo "# -l/--logs <liczba_plikow>: Tworzy określoną liczbę plików log."
+  echo "# -h/--help: Wyświetla wszystkie dostępne opcje."
 
-elif [[ $1 == "--init" ]]; then
-  git clone https://github.com/Sterus3/Lab4 && cd lab4 && export PATH=$PATH:$(pwd)
+elif [[ $1 == "-e" || $1 == "--error" ]]; then
+  wprowadzona_liczba=$2
+
+  if [[ -z $wprowadzona_liczba ]]; then
+    wprowadzona_liczba=100
+  fi
+
+  for ((i=1; i<=$wprowadzona_liczba; i++)); do
+    nazwa_pliku="error$i/error$i.txt"
+    mkdir "error$i"
+    echo "Nazwa pliku: $nazwa_pliku" > $nazwa_pliku
+    echo "Nazwa skryptu: skrypt.sh" >> $nazwa_pliku
+    echo "Data utworzenia: $(date +%d-%m-%Y)" >> $nazwa_pliku
+  done
+
 fi
 
