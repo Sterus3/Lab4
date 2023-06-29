f4909f837ab831b8191c84087307bce33ded0de8
Dsw51305 <dsw51305@student.dsw.edu.pl>
2023-06-29 07:38:31 +0200

    Dodano flagę --logs, która utworzy 100 plików logx.txt, x - numer pliku od 1 do 100, w każdym będzie zapisana jego nazwa, nazwa skryptu przez którą został utworzony oraz data.

--- skrypt.sh
+++ skrypt.sh
@@ -2,4 +2,14 @@
 
 if [[ $1 == "--date" ]]; then
   echo "Dzisiejsza data: $(date +%d-%m-%Y)"
+
+elif [[ $1 == "--logs" ]]; then
+  for ((i=1; i<=100; i++)); do
+    nazwa_pliku="log$i.txt"
+    echo "Nazwa pliku: $nazwa_pliku" > $nazwa_pliku
+    echo "Nazwa skryptu: skrypt.sh" >> $nazwa_pliku
+    echo "Data utworzenia: $(date +%d-%m-%Y)" >> $nazwa_pliku
+  done
+
 fi
+
