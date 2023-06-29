1612c472ff5c77b16d3b72228e7cfa4cfd5b937b
Dsw51305 <dsw51305@student.dsw.edu.pl>
2023-06-29 07:49:03 +0200

    Zaktualizowano flagę --logs, o konieczność wpisania ilości plików do utworzenia.

--- skrypt.sh
+++ skrypt.sh
@@ -4,12 +4,18 @@ if [[ $1 == "--date" ]]; then
   echo "Dzisiejsza data: $(date +%d-%m-%Y)"
 
 elif [[ $1 == "--logs" ]]; then
-  for ((i=1; i<=100; i++)); do
+  wprowadzona_liczba=$2
+
+  if [[ -z $wprowadzona_liczba ]]; then
+    echo "Nie podano liczby plików. Po użyciu funkcji --logs, wpisz ilość plików, które chcesz utworzyć"
+    exit 1
+  fi
+
+  for ((i=1; i<=$wprowadzona_liczba; i++)); do
     nazwa_pliku="log$i.txt"
     echo "Nazwa pliku: $nazwa_pliku" > $nazwa_pliku
     echo "Nazwa skryptu: skrypt.sh" >> $nazwa_pliku
     echo "Data utworzenia: $(date +%d-%m-%Y)" >> $nazwa_pliku
   done
-
 fi
 
